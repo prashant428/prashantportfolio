1feee36854a5890fdd186175dcacd296ff273a60
Prashant Sharma <prashant428@gmail.com>
2021-10-19 18:34:30 -0400

    configured remote database to include oontacts collection

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,8 @@
 module.exports =
 {
     
-    "URI": "mongodb://localhost/book_store",
-    //"URI": 
-   // "mongodb+srv://prashant:NiXrLI8bTqq7sEEC@databaseserver.edclv.mongodb.net/book_store?retryWrites=true&w=majority",
+    //"URI": "mongodb://localhost/book_store",
+    "URI": 
+   "mongodb+srv://prashant:NiXrLI8bTqq7sEEC@databaseserver.edclv.mongodb.net/book_store?retryWrites=true&w=majority",
     "Secret" : 'SomeSecret'
 }
